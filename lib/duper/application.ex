f963d7dd2830b35587a4c0147a435f193fd8e663
Hugo Hernani <hhernanni@gmail.com>
2021-02-14 23:17:37 -0300

    Add Duper.PathFinder server which uses DirWalker hex package to traverse files

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -8,7 +8,8 @@ defmodule Duper.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      Duper.Results
+      Duper.Results,
+      {Duper.PathFinder, "."}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
---
+++ lib/duper/path_finder.ex
@@ -0,0 +1,31 @@
+defmodule Duper.PathFinder do
+  use GenServer
+
+  @me PathFinder
+
+  # API
+
+  def start_link(root) do
+    GenServer.start_link(__MODULE__, root, name: @me)
+  end
+
+  def next_path() do
+    GenServer.call(@me, :next_path)
+  end
+
+  # SERVER
+
+  def init(path) do
+    DiwWalker.start_link(path)
+  end
+
+  def handle_call(:next_path, _from, dir_walker) do
+    path =
+      case DirWalker.next(dir_walker) do
+        [path] -> path
+        other -> other
+      end
+
+    {:reply, path, dir_walker}
+  end
+end
